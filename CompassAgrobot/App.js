faa70f529a011de38f4c1a6ab7d9e7a5e8c5085d
Lucas <lucasgrafimar@gmail.com>
2022-07-11 21:53:37 -0300

    added socket.io

--- CompassAgrobot/App.js
+++ CompassAgrobot/App.js
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
-// import io from 'socket.io-client';
+import io from 'socket.io-client';
 import {magnetometer} from 'react-native-sensors';
 import LPF from 'lpf';
 
@@ -13,41 +13,43 @@ const App = () => {
     } else {
       angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
     }
-    // angle = angle - 90 >= 0 ? angle - 90 : angle + 271;
     angle = angle - 84;
     if (angle < 0) angle = 360 + angle;
     if (angle >= 90 && angle <= 300) angle = angle - 16;
     console.log(Math.round(LPF.next(angle)));
+    client.emit('compass_update', Math.round(LPF.next(angle)));
   });
 
-  // const [client, setClient] = useState(io(''));
-  // const [connected, setConnected] = useState(false);
+  const [client, setClient] = useState(io(''));
+  const [connected, setConnected] = useState(false);
 
-  // const connect = () => {
-  //   client.close();
-  //   const newClient = io('http://192.168.0.107:3000/%27);
-  //   newClient.on('disconnect', () => {
-  //     setConnected(false);
-  //     console.log('Cliente desconectado');
-  //   });
+  const connect = () => {
+    client.close();
+    const newClient = io('http://192.168.0.107:3000');
+    newClient.on('disconnect', () => {
+      setConnected(false);
+      console.log('Cliente desconectado');
+    });
 
-  //   newClient.on('connect', () => {
-  //     setConnected(true);
-  //     console.log('Cliente conectado');
-  //   });
+    newClient.on('connect', () => {
+      setConnected(true);
+      console.log('Cliente conectado');
+    });
 
-  //   setClient(newClient);
-  // };
-  // useEffect(() => {
-  //   connect();
-  // }, []);
+    setClient(newClient);
+  };
+  useEffect(() => {
+    connect();
+  }, []);
 
-  // const emit = () => {
-  //   client.emit('auto_mode_params_update', data);
-  // };
+  const emit = () => {
+    console.log('uhasuash');
+
+    client.emit('compass_update', {data: 'data'});
+  };
   return (
     <View>
-      <Text>asasa $</Text>
+      <Text onPress={emit}>asasa $</Text>
     </View>
   );
 };
